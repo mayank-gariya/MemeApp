4e489fbe9b61b99cb7135030492d4369d92ea949
mayank-ml_dev <mayankgariya482@gmail.com>
2026-09-13 18:29:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,5 +100,5 @@ def customise_model():
 
         return jsonify({"error":str(e)}),400
     
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
